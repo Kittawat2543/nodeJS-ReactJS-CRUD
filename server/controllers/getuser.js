7e935a447286c226fe71106f2f24e8f161f58ff2
Kittawat <kittawatnot1725@gmail.com>
2024-07-08 17:49:51 +0700

    update manage role

--- server/controllers/getuser.js
+++ server/controllers/getuser.js
@@ -16,3 +16,20 @@ exports.list = async (req, res) => {
     res.status(500).send("currentUser Error");
   }
 };
+
+exports.changerole = async (req, res) => {
+  try {
+    const { id , role } = req.body.data
+
+    const user = await modelUser
+      .findOneAndUpdate({_id:id},{role:role},{new:true})
+      .select("-password")
+      .exec();
+
+      res.send(user);
+      
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("currentUser Error");
+  }
+};
--- server/routes/routeUser.js
+++ server/routes/routeUser.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  list
-} = require("../controllers/getuser");
+const { list,changerole } = require("../controllers/getuser");
 const { auth, adminCheck } = require("../middleware/middlewareAuth");
 
 
 router.get("/user", auth, adminCheck, list);
+router.post("/change-role", auth, adminCheck, changerole);
+
 
 module.exports = router;
