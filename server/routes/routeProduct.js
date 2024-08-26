ca10ba8de4d6e946776afd821c757dbd3c71cc9f
Kittawat <kittawatnot1725@gmail.com>
2024-08-26 11:31:43 +0700

    fix bug

--- server/routes/routeProduct.js
+++ server/routes/routeProduct.js
@@ -13,14 +13,14 @@ const {
 const { auth } = require("../middleware/middlewareAuth");
 const { upload } = require("../middleware/middlewareUpload")
 
-router.get("/product", auth, list);
+router.get("/product", list);
 
-router.get("/product/:id", auth, read);
+router.get("/product/:id", read);
 
-router.post("/product", auth, upload, create);
+router.post("/product", upload, create);
 
-router.put("/product/:id", auth, upload, update);
+router.put("/product/:id", upload, update);
 
-router.delete("/product/:id", auth, remove);
+router.delete("/product/:id", remove);
 
 module.exports = router;
